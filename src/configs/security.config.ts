8d8fb3701d7b8a0edc7b83318527a1b5d4a050ad
KimAn <an.vu@icetea.io>
2025-07-14 17:58:00 +0700

    feat: securiry conffig

--- src/configs/security.config.ts
+++ src/configs/security.config.ts
@@ -1,14 +1,14 @@
-import { ConfigType, registerAs } from '@nestjs/config'
+import { ConfigType, registerAs } from '@nestjs/config';
 
-import { env, envNumber } from '../global/env'
+import { env, envNumber } from '../global/env';
 
-export const securityRegToken = 'security'
+export const securityRegToken = 'security';
 
 export const SecurityConfig = registerAs(securityRegToken, () => ({
-  jwtSecret: env('JWT_SECRET'),
-  jwtExprire: envNumber('JWT_EXPIRE'),
-  refreshSecret: env('REFRESH_TOKEN_SECRET'),
-  refreshExpire: envNumber('REFRESH_TOKEN_EXPIRE'),
-}))
+  jwtSecret: env('JWT_SECRET', 'secret'),
+  jwtExprire: envNumber('JWT_EXPIRE', 3600),
+  refreshSecret: env('REFRESH_TOKEN_SECRET', 'refresh_secret'),
+  refreshExpire: envNumber('REFRESH_TOKEN_EXPIRE', 86400),
+}));
 
-export type ISecurityConfig = ConfigType<typeof SecurityConfig>
+export type ISecurityConfig = ConfigType<typeof SecurityConfig>;
