6de157f251aeb1593505305c63f1aeee369ba9ae
KimAn <an.vu@icetea.io>
2025-10-09 11:25:01 +0700

    refactor: update database credentials to use 'admin' and change database name to 'myapp_db'

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -10,9 +10,9 @@ const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: env('DB_HOST', 'localhost'),
   port: envNumber('DB_PORT', 32771),
-  username: env('DB_USERNAME', 'postgres'),
-  password: env('DB_PASSWORD', 'kiman0102'),
-  database: env('DB_DATABASE', 'MyJobAPI'),
+  username: env('DB_USERNAME', 'admin'),
+  password: env('DB_PASSWORD', 'admin'),
+  database: env('DB_DATABASE', 'myapp_db'),
   synchronize: true,
   // ssl: {
   //   rejectUnauthorized: false, // Nếu bạn không có chứng chỉ SSL hợp lệ
